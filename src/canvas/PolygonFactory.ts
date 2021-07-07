43800e564a49bc0a762fa070816d92d497658197
Liam <81374620+LiamMCode@users.noreply.github.com>
2021-07-07 16:05:41 +0100

    units can now be moved

--- src/canvas/PolygonFactory.ts
+++ src/canvas/PolygonFactory.ts
@@ -17,17 +17,15 @@ export enum UnitSize {
 
 export class PolygonFactory {
     public createRectangle(unit: WidgetUnitData): Polygon {
-        console.log(unit);
         const { dimensions } = unit;
 
-        const height = dimensions.getHeight();
         const width = dimensions.getWidth();
         const depth = dimensions.getDepth();
 
-        const topLeft = new Point(0, height, depth);
-        const topRight = new Point(width, height, depth);
-        const bottomRight = new Point(width, height, 0);
-        const bottomLeft = new Point(0, height, 0);
+        const topLeft = new Point(0, 0, depth);
+        const topRight = new Point(width, 0, depth);
+        const bottomRight = new Point(width, 0, 0);
+        const bottomLeft = new Point(0, 0, 0);
 
         return new Polygon(dimensions, [topLeft, topRight, bottomRight, bottomLeft]);
     }
